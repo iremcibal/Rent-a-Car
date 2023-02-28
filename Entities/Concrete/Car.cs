08c762d5ac0b756eb52dfae829a958f2e1748338
Asus <Asus@DESKTOP-6HHUABE>
2023-03-01 00:15:22 +0300

    Add project files.

---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Car : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BrandId { get; set; }
+        public int ColorId { get; set; }
+        public int CarImageId { get; set; }
+        public string Description { get; set; }
+        public int DailyPrice { get; set; }
+        public int ModelYear { get; set; }
+        public int MinFindeksScore { get; set; }
+        public string Plate { get; set; }
+        public string Kilometer { get; set; }
+        public virtual Brand brand { get; set; }
+        public virtual Color color { get; set; }
+        public virtual CarImage carImage { get; set; }
+
+    }
+}
---
+++ Entities/Concrete/Findeks.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Findeks : IEntity
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string NationalIdentity { get; set; }
+        public int Score { get; set; }
+        public virtual Customer Customer { get; set; }
+
+    }
+}
